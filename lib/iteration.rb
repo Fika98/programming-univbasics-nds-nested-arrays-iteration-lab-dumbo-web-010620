866e98ce18de65779824cfcecc69dbf1b3ec4db3
Firuz Afzalov <afzalov98@gmail.com>
2020-01-02 03:48:19 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,7 +6,7 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-  ow_index = 0
+  row_index = 0
     
     while row_index < array.count do
         new_array = []    
